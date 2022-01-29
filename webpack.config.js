77d1f738b5d6d8634f6276d5b502ce4b7c6ee26e
Sergey <Sergey-871@mail.ru>
2022-01-29 17:13:52 +0300

    Настройка сервера для разработки

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,9 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
     },
-    devtool: 'source-map'   /* карты исходников */
+    devtool: 'source-map',   /* карты исходников */
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),     /* откуда смотреть исходники */
+        watchContentBase: true  /* перезагружать страницу при изменении? */
+    }
 };
