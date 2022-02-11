9a11b2162d36e44b20eb54598e6ffc763ebd6875
Sergey <Sergey-871@mail.ru>
2022-02-11 23:23:27 +0300

    Код разбит на модули

---
+++ src/view/board-view.js
@@ -0,0 +1,19 @@
+export const createBoardTemplate = () => (
+    `<section class="films">
+    <section class="films-list">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+      <div class="films-list__container"></div>
+      <button class="films-list__show-more">Show more</button>
+    </section>
+
+    <section class="films-list films-list--extra">
+      <h2 class="films-list__title">Top rated</h2>
+      <div class="films-list__container"></div>
+    </section>
+
+    <section class="films-list films-list--extra">
+      <h2 class="films-list__title">Most commented</h2>
+      <div class="films-list__container"></div>
+    </section>
+  </section>`
+);
